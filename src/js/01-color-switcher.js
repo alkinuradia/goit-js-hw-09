85b5b3db0ddb7b7d05e6d03b5a20b34dcdaf3920
alkinuradia <112646930+alkinuradia@users.noreply.github.com>
2023-03-13 16:20:40 +0200

    firs-hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let idInterval = null;
+startBtn.disabled = false;
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', ClearChangeColor);
+
+function changeColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  idInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function ClearChangeColor() {
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(idInterval);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
